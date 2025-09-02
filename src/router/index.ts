12d8c06913c26bd69e2290f37f593c1df376ea58
SnowDream39 <759723417@qq.com>
2025-09-02 17:28:13 +0800

    改用动态加载component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import Board from '../views/Board.vue'
-import Song from '../views/Song.vue'
-import Search from '../views/Search.vue'
-import Artist from '../views/Artist.vue'
-import Pool from '../views/Pool.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/board/:boardId/:issue?', component: Board },
-  { path: '/about', component: About },
-  { path: '/song/:id', component: Song },
-  { path: '/search', component: Search },
-  { path: '/artist/:type/:id', component: Artist } ,
-  { path: '/pool', component: Pool },
+  { path: '/', component: () => import('../views/Home.vue') },
+  { path: '/board/:boardId/:issue?', component: () => import('../views/Board.vue') },
+  { path: '/about', component: () => import('../views/About.vue') },
+  { path: '/song/:id', component: () => import('../views/Song.vue') },
+  { path: '/search', component: () => import('../views/Search.vue') },
+  { path: '/artist/:type/:id', component: () => import('../views/Artist.vue') } ,
+  { path: '/pool', component: () => import('../views/Pool.vue') },
   { path: '/top5', component: () => import('../views/Top.vue')},
   { path: '/work', component: () => import('../views/Work.vue')},
   { path: '/user', component: () => import('../views/User.vue')},
